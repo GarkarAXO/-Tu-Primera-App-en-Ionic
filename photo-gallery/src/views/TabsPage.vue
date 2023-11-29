7a7768ee5d2c474dc2e05b51a2d71a4bfee03e59
GarkarAXO <99782340+GarkarAXO@users.noreply.github.com>
2023-11-29 15:08:00 +0000

    S04E99 Proyecto Final

--- photo-gallery/src/views/TabsPage.vue
+++ photo-gallery/src/views/TabsPage.vue
@@ -13,10 +13,6 @@
         <ion-label>Fotos</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
-        </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
@@ -24,5 +20,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { images, square, home } from 'ionicons/icons';
+import { images, home } from 'ionicons/icons';
 </script>

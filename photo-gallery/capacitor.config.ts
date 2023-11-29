e3be20053e0f0b758f5bdda9e4b41f57fd7a68bb
GarkarAXO <99782340+GarkarAXO@users.noreply.github.com>
2023-11-29 07:40:46 +0000

    final

--- photo-gallery/capacitor.config.ts
+++ photo-gallery/capacitor.config.ts
@@ -2,15 +2,26 @@ import { CapacitorConfig } from '@capacitor/cli';
 import { isPlatform } from '@ionic/vue';
 import { Capacitor } from '@capacitor/core';
 
-
-
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    LiveUpdates: {
+      appId: '042a1261',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
   }
 };
 
 export default config;
+
+
+
+
+
